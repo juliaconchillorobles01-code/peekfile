9d691444f5a8efd1fe2ae41fccbb05476d17d554
Júlia <julia.conchillorobles01@estudiant.upf.edu>
2025-11-05 23:15:34 +0100

    Part 2 homework 2

--- countlines.sh
+++ countlines.sh
@@ -1,6 +1,7 @@
-p=$(wc -l < $1)
+for file in $@; do
+p=$(wc -l < $file)
 
-echo "File name: $1"
+echo "File name: $file"
 
 if [[ $p == 0 ]]; then
 echo "it has 0 lines" 
@@ -11,3 +12,4 @@ echo "it has 1 line"
 else 
 echo "it has more than 1 line"
 fi
+done
