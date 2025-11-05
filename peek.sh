5416054048858415ec01106041d1a843e4b701d4
Júlia <julia.conchillorobles01@estudiant.upf.edu>
2025-11-05 22:31:35 +0100

    Part 2 of homework 1

--- peek.sh
+++ peek.sh
@@ -1,11 +1,11 @@
+flines=$( wc -l < "$1")
+ulines=$((2 * $2))
 
-if [[ -z $2 ]]; then
-lines=3
-else
-lines=$2
+if [[ $flines -le $ulines ]]; then 
+cat $1
+else 
+echo "warning, there are not 2X lines or less" 
+head -n$2 $1 
+echo "..."
+tail -n$2 $1
 fi
-
-head -n$lines $1
-echo "..." 
-tail -n$lines $1 
-
